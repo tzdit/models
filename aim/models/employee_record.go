29d79b85b3041e5921271c7de6ad082e904ce354
cngirwa <cngirwa2007@gmail.com>
2021-10-27 21:49:50 +0300

    Update employee_record.go

--- aim/models/employee_record.go
+++ aim/models/employee_record.go
@@ -29,3 +29,30 @@ type EmployeeRecord struct {
 	CreatedBy         int       `json:"created_by,omitempty"`
 	CreatedAt         time.Time `json:"created_at,omitempty"`
 }
+
+type EmployeeRecordFull struct {
+	Id                   int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	PFNo                 string    `json:"pf_no" form:"pf_no" validate:"required"`
+	FirstName            string    `json:"first_name" form:"first_name" validate:"required"`
+	MiddleName           string    `json:"middle_name" form:"middle_name" validate:"required"`
+	LastName             string    `json:"last_name" form:"last_name" validate:"required"`
+	SubstantivePostTitle string    `json:"substantive_post_title" form:"substantive_post_id" validate:"required"`
+	DutyPostTitle        string    `json:"duty_post_title" form:"duty_post_id" validate:"required"`
+	DepartmentTitle      string    `json:"department_title" form:"department_id" validate:"required"`
+	DoB                  time.Time `json:"dob" form:"dob" validate:"required"`
+	PlaceOfBirth         string    `json:"place_of_birth" form:"place_of_birth" validate:"required"`
+	DateEmployed         time.Time `json:"date_employed" form:"date_employed" validate:"required"`
+	DateToRetire         time.Time `json:"date_to_retire" form:"date_to_retire" validate:"required"`
+	PostalAddress        string    `json:"postal_address" form:"postal_address" validate:"required"`
+	PhysicalAddress      string    `json:"physical_address" form:"physical_address" validate:"required"`
+	PlaceOfDomicile      string    `json:"place_of_domicile" form:"place_of_domicile" validate:"required"`
+	MaritalStatus        string    `json:"marital_status" form:"marital_status_id" validate:"required"`
+	Religion             string    `json:"religion" form:"religion_id" validate:"required"`
+	EmploymentType       string    `json:"employment_type" form:"action_type_id" validate:"required"`
+	ScaleCode            string    `json:"scale_code" form:"salary_scale_id" validate:"required"`
+	MyUserId             int       `json:"my_user_id" form:"my_user_id" validate:"required"`
+	AppointmentDate      time.Time `json:"appointment_date" form:"appointment_date" validate:"required"`
+	ConfirmationDate     time.Time `json:"confirmation_date" form:"confirmation_date" validate:"required"`
+	CreatedBy            int       `json:"created_by,omitempty"`
+	CreatedAt            time.Time `json:"created_at,omitempty"`
+}
