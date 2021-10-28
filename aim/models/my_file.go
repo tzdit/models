a46d31f9fd7a56f0a1bcb95a07aefbb7f7711f74
cngirwa <cngirwa2007@gmail.com>
2021-10-28 11:28:36 +0300

    Update my_file.go

--- aim/models/my_file.go
+++ aim/models/my_file.go
@@ -12,3 +12,14 @@ type MyFile struct {
 	CreatedBy    int       `json:"created_by,omitempty"`
 	CreatedAt    time.Time `json:"created_at"`
 }
+
+type MyFileFull struct {
+	Id         int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	FileName   string    `json:"file_name" form:"file_name" validate:"required"`
+	FileNumber string    `json:"file_number" form:"file_number" validate:"required"`
+	TypeName   string    `json:"my_file_type_id,omitempty" form:"my_file_type_id" validate:"omitempty,numeric"`
+	Status     string    `json:"file_status_id,omitempty" form:"file_status_id" validate:"omitempty,numeric"`
+	StoredBy   int       `json:"stored_by,omitempty" form:"stored_by" validate:"omitempty,numeric"`
+	CreatedBy  int       `json:"created_by,omitempty"`
+	CreatedAt  time.Time `json:"created_at"`
+}
