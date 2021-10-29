82b9861b60a72a808c236a6c003ad727d4c899af
cngirwa <cngirwa2007@gmail.com>
2021-10-29 22:56:20 +0300

    added 2 models
    added

--- aim/models/leave_approval.go
+++ aim/models/leave_approval.go
@@ -2,12 +2,22 @@ package models
 
 import "time"
 
-type LeaveApproval struct{
-	Id	int	`json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
-	LeaveDetailId    int		`json:"leave_detail_id,omitempty" form:"leave_detail_id" validate:"omitempty,numeric"`
-	DutyPostId    int		`json:"duty_post_id,omitempty" form:"duty_post_id" validate:"omitempty,numeric"`
-	ActionTypeId    int		`json:"action_type_id,omitempty" form:"action_type_id" validate:"omitempty,numeric"`
-	Minutes    string		`json:"minutes" form:"minutes" validate:"required"`
-	CreatedBy    int		`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
-	CreatedAt    time.Time		`json:"created_at" form:"created_at" validate:"required"`
+type LeaveApproval struct {
+	Id            int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	LeaveDetailId int       `json:"leave_detail_id,omitempty" form:"leave_detail_id" validate:"omitempty,numeric"`
+	DutyPostId    int       `json:"duty_post_id,omitempty" form:"duty_post_id" validate:"omitempty,numeric"`
+	ActionTypeId  int       `json:"action_type_id,omitempty" form:"action_type_id" validate:"omitempty,numeric"`
+	Minutes       string    `json:"minutes" form:"minutes" validate:"required"`
+	CreatedBy     int       `json:"created_by,omitempty"`
+	CreatedAt     time.Time `json:"created_at"`
+}
+
+type LeaveApprovalFull struct {
+	Id            int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	LeaveDetailId int       `json:"leave_detail_id,omitempty" form:"leave_detail_id" validate:"omitempty,numeric"`
+	DutyPostTitle string    `json:"duty_post_id,omitempty" form:"duty_post_id" validate:"required"`
+	ActionName    string    `json:"action_type_id,omitempty" form:"action_type_id" validate:"omitempty,numeric"`
+	Minutes       string    `json:"minutes" form:"minutes" validate:"required"`
+	CreatedBy     int       `json:"created_by,omitempty"`
+	CreatedAt     time.Time `json:"created_at"`
 }
--- aim/models/vehicle_maintenance_part.go
+++ aim/models/vehicle_maintenance_part.go
@@ -1,17 +1,30 @@
-
 package models
 
 import "time"
 
-type VehicleMaintenancePart struct{
-	Id					int		`json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
-	VehicleInspectionId int		`json:"vehicle_inspection_id,omitempty" form:"vehicle_inspection_id" validate:"omitempty,numeric"`
-	PartName    		string	`json:"part_name" form:"part_name" validate:"required"`
-	PartDescriptions    string	`json:"part_descriptions" form:"part_descriptions" validate:"required"`
-	Quantity    		float64	`json:"quantity" form:"quantity" validate:"required"`
-	UnitPrice    		float64	`json:"unit_price" form:"unit_price" validate:"required"`
-	VendorId    		int		`json:"vendor_id,omitempty" form:"vendor_id" validate:"omitempty,numeric"`
-	TotalPrice   	 	float64	`json:"total_price" form:"total_price" validate:"required"`
-	CreatedBy    		int		`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
-	CreatedAt   	 	time.Time		`json:"created_at" form:"created_at" validate:"required"`
+type VehicleMaintenancePart struct {
+	Id                  int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	VehicleInspectionId int       `json:"vehicle_inspection_id,omitempty" form:"vehicle_inspection_id" validate:"omitempty,numeric"`
+	PartName            string    `json:"part_name" form:"part_name" validate:"required"`
+	PartDescriptions    string    `json:"part_descriptions" form:"part_descriptions" validate:"required"`
+	Quantity            float64   `json:"quantity" form:"quantity" validate:"required"`
+	UnitPrice           float64   `json:"unit_price" form:"unit_price" validate:"required"`
+	VendorId            int       `json:"vendor_id,omitempty" form:"vendor_id" validate:"omitempty,numeric"`
+	TotalPrice          float64   `json:"total_price" form:"total_price" validate:"required"`
+	CreatedBy           int       `json:"created_by,omitempty"`
+	CreatedAt           time.Time `json:"created_at"`
 }
+
+type VehicleMaintenancePartFull struct {
+	Id                  int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	VehicleInspectionId int       `json:"vehicle_inspection_id,omitempty" form:"vehicle_inspection_id" validate:"omitempty,numeric"`
+	PartName            string    `json:"part_name" form:"part_name" validate:"required"`
+	PartDescriptions    string    `json:"part_descriptions" form:"part_descriptions" validate:"required"`
+	Quantity            float64   `json:"quantity" form:"quantity" validate:"required"`
+	UnitPrice           float64   `json:"unit_price" form:"unit_price" validate:"required"`
+	VendorName          string    `json:"vendor_id,omitempty" form:"vendor_id" validate:"omitempty,numeric"`
+	TotalPrice          float64   `json:"total_price" form:"total_price" validate:"required"`
+	CreatedBy           int       `json:"created_by,omitempty"`
+	CreatedAt           time.Time `json:"created_at"`
+}
+
