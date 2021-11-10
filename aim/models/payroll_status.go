484bd5393f8008e3d73a23bad5c604403d8b5802
Stephen Wambura <culdive@gmail.com>
2021-11-10 21:21:10 +0800

    Update payroll_status.go

--- aim/models/payroll_status.go
+++ aim/models/payroll_status.go
@@ -5,7 +5,7 @@ type PayrollStatus struct{
 	Id	int	`json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
 	Status    string		`json:"status" form:"status" validate:"required"`
 	Descriptions    string		`json:"descriptions" form:"descriptions" validate:"required"`
-	CreatedBy    int		`json:"created_by,omitempty"  validate:"omitempty,numeric"`
-	CreatedAt    time.Time		`json:"created_at" validate:"required"`
+	CreatedBy    int       `json:"created_by,omitempty"`
+	CreatedAt    time.Time `json:"created_at,omitempty"`
 
 }
