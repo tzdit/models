ffa4bff71b9a4e959760e69b7116938f1a371fb6
Stephen Wambura <culdive@gmail.com>
2021-11-10 16:06:16 +0300

    updated models

---
+++ payroll_status.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type PayrollStatus struct {
+	Id           int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	Status       string    `json:"status" form:"status" validate:"required"`
+	Descriptions string    `json:"descriptions" form:"descriptions" validate:"required"`
+	CreatedBy    int       `json:"created_by,omitempty"`
+	CreatedAt    time.Time `json:"created_at,omitempty"`
+}
