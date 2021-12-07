eafce1bec61d14d734ed8d3f93ca0483cbd825a7
Rama Mndeme <ramamndeme@gmail.com>
2021-12-07 13:28:56 +0300

    Update allegation_action.go

--- aim/models/allegation_action.go
+++ aim/models/allegation_action.go
@@ -12,3 +12,13 @@ type AllegationAction struct {
 	CreatedBy         int       `json:"created_by,omitempty"`
 	CreatedAt         time.Time `json:"created_at,omitempty"`
 }
+
+type AllegationActionFull struct {
+	Id                int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	Name              string    `json:"name" form:"name" validate:"required"`
+	Descriptions      string    `json:"descriptions" form:"descriptions" validate:"required"`
+	AllegationLevelName string       `json:"allegation_level_id,omitempty" form:"allegation_level_id" validate:"omitempty,numeric"`
+	AllegationId      string       `json:"allegation_id,omitempty" form:"allegation_id" validate:"omitempty,numeric"`
+	CreatedBy         int       `json:"created_by,omitempty"`
+	CreatedAt         time.Time `json:"created_at,omitempty"`
+}
