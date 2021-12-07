d6610fc9e642cf6b4dc7a42d2268e1e3dfb41b27
Rama Mndeme <ramamndeme@gmail.com>
2021-12-07 13:39:42 +0300

    Update allegation_level.go

--- aim/models/allegation_level.go
+++ aim/models/allegation_level.go
@@ -11,3 +11,11 @@ type AllegationLevel struct {
 	CreatedBy    int       `json:"created_by,omitempty"`
 	CreatedAt    time.Time `json:"created_at,omitempty"`
 }
+type AllegationLevelFull struct {
+	Id           int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	Name         string    `json:"name" form:"name" validate:"required"`
+	Descriptions string    `json:"descriptions" form:"descriptions" validate:"required"`
+	MyUserRoleId int       `json:"my_user_role_id,omitempty" form:"my_user_role_id" validate:"omitempty,numeric"`
+	CreatedBy    int       `json:"created_by,omitempty"`
+	CreatedAt    time.Time `json:"created_at,omitempty"`
+}
