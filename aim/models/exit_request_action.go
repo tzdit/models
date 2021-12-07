0346b542ba77e050d4ac70fce8daf2919955c021
Rama Mndeme <ramamndeme@gmail.com>
2021-12-07 15:29:07 +0300

    Update exit_request_action.go

--- aim/models/exit_request_action.go
+++ aim/models/exit_request_action.go
@@ -13,3 +13,16 @@ type ExitRequestAction struct{
 	CreatedAt    		time.Time		`json:"created_at" form:"created_at" validate:"required"`
 
 }
+
+type ExitRequestActionFull struct{
+	Id					int	`json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	ExitRequest    	string		`json:"exit_request_id,omitempty" form:"exit_request_id" validate:"omitempty,numeric"`
+	PreviousId    		string		`json:"previous_id,omitempty" form:"previous_id" validate:"omitempty,numeric"`
+	NextId    			string		`json:"next_id,omitempty" form:"next_id" validate:"omitempty,numeric"`
+	ActionType    	string		`json:"action_type_id,omitempty" form:"action_type_id" validate:"omitempty,numeric"`
+	Minute    			string		`json:"minute" form:"minute" validate:"required"`
+	CreatedBy    		int		`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
+	CreatedAt    		time.Time		`json:"created_at" form:"created_at" validate:"required"`
+
+}
+
