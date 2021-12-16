1086db04d6c8c26e6af3a1c96205d3d5e873e065
Rama Mndeme <ramamndeme@gmail.com>
2021-12-16 14:11:19 +0300

    Update vehicle_maintenance_request.go

--- aim/models/vehicle_maintenance_request.go
+++ aim/models/vehicle_maintenance_request.go
@@ -14,3 +14,15 @@ type VehicleMaintenanceRequest struct{
 	CreatedBy    		int			`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
 	CreatedAt    		time.Time	`json:"created_at" form:"created_at" validate:"required"`
 }
+
+type VehicleMaintenanceRequestFull struct {
+	Id					int			`json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	VehicleId    		string			`json:"vehicle_id,omitempty" form:"vehicle_id" validate:"omitempty,numeric"`
+	DateRequested    	time.Time	`json:"date_requested" form:"date_requested" validate:"required"`
+	ActionTypeId    	string			`json:"action_type_id,omitempty" form:"action_type_id" validate:"omitempty,numeric"`
+	MaintananceReasons  string		`json:"maintanance_reasons" form:"maintanance_reasons" validate:"required"`
+	VehicleStatusId    	string			`json:"vehicle_status_id,omitempty" form:"vehicle_status_id" validate:"omitempty,numeric"`
+	AttachmentId    	string			`json:"attachment_id,omitempty" form:"attachment_id" validate:"omitempty,numeric"`
+	CreatedBy    		int			`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
+	CreatedAt    		time.Time	`json:"created_at" form:"created_at" validate:"required"`
+}
