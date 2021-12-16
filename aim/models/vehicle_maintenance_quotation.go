bb3af361a88e2ba5540efca52999de22010e7b89
Rama Mndeme <ramamndeme@gmail.com>
2021-12-16 12:52:56 +0300

    Update vehicle_maintenance_quotation.go

--- aim/models/vehicle_maintenance_quotation.go
+++ aim/models/vehicle_maintenance_quotation.go
@@ -14,8 +14,8 @@ type VehicleMaintenanceQuotation struct{
 
 type VehicleMaintenanceQuotationFull struct {
 	Id						int	`json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
-	VehicleInspectionId    	int		`json:"vehicle_inspection_id,omitempty" form:"vehicle_inspection_id" validate:"omitempty,numeric"`
-	VendorId    			int		`json:"vendor_id,omitempty" form:"vendor_id" validate:"omitempty,numeric"`
+	VehicleInspectionId    	string		`json:"vehicle_inspection_id,omitempty" form:"vehicle_inspection_id" validate:"omitempty,numeric"`
+	VendorId    			string		`json:"vendor_id,omitempty" form:"vendor_id" validate:"omitempty,numeric"`
 	TotalPrice    			string		`json:"total_price" form:"total_price" validate:"required"`
 	IsApproved    			string		`json:"is_approved" form:"is_approved" validate:"required"`
 	CreatedBy    			int		`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
