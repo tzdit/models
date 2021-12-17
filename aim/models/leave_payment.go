3d88c9a2d9bbdb3c78716dd2224c1449ba773dd9
Rama Mndeme <ramamndeme@gmail.com>
2021-12-17 11:16:45 +0300

    Update leave_payment.go

--- aim/models/leave_payment.go
+++ aim/models/leave_payment.go
@@ -12,3 +12,14 @@ type LeavePayment struct{
 	CreatedBy    				int			`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
 	CreatedAt    				time.Time	`json:"created_at" form:"created_at" validate:"required"`
 }
+
+type LeavePaymentFull struct{
+	Id							int		`json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	BusinessProcessInstanceId   string		`json:"business_process_instance_id,omitempty" form:"business_process_instance_id" validate:"omitempty,numeric"`
+	LeaveDetailId    			string		`json:"leave_detail_id,omitempty" form:"leave_detail_id" validate:"omitempty,numeric"`
+	LeavePaymentItemId    		string		`json:"leave_payment_item_id,omitempty" form:"leave_payment_item_id" validate:"omitempty,numeric"`
+	Quantity    				string		`json:"quantity" form:"quantity" validate:"required"`
+	TotalPrice    				string		`json:"total_price" form:"total_price" validate:"required"`
+	CreatedBy    				int			`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
+	CreatedAt    				time.Time	`json:"created_at" form:"created_at" validate:"required"`
+}
