363a7979569622a1ad09837f982b658d1251c639
Rama Mndeme <ramamndeme@gmail.com>
2021-12-17 11:30:26 +0300

    Update allegations.go

--- aim/models/allegations.go
+++ aim/models/allegations.go
@@ -12,3 +12,11 @@ type Allegation struct {
 	CreatedBy    int       `json:"created_by,omitempty"`
 	CreatedAt    time.Time `json:"created_at,omitempty"`
 }
+
+type AllegationFull struct {
+	Id           int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	ActionTypeId string       `json:"action_type_id,omitempty" form:"action_type_id" validate:"omitempty,numeric"`
+	DateAccused  time.Time `json:"date_accused,omitempty" form:"date_accused" validate:"required"`
+	CreatedBy    int       `json:"created_by,omitempty"`
+	CreatedAt    time.Time `json:"created_at,omitempty"`
+}
