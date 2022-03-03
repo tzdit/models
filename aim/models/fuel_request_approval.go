bf3f784e336316e69e97fa0d26b67a0cf5539782
cngirwa <cngirwa2007@gmail.com>
2022-03-03 11:51:34 +0300

    updates

--- aim/models/fuel_request_approval.go
+++ aim/models/fuel_request_approval.go
@@ -11,3 +11,13 @@ type FuelRequestApproval struct{
 	CreatedBy    int		`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
 	CreatedAt    time.Time		`json:"created_at" form:"created_at" validate:"required"`
 }
+
+type FuelRequestApprovalFull struct{
+	Id	int	`json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	FuelRequestTitle    string		`json:"fuel_request_id,omitempty" form:"fuel_request_id" validate:"omitempty,numeric"`
+	ApprovingOfficer    string		`json:"approving_officer" form:"approving_officer" validate:"required"`
+	FuelRequestActionTitle    string		`json:"fuel_request_action_id,omitempty" form:"fuel_request_action_id" validate:"omitempty,numeric"`
+	Minutes    string		`json:"minutes" form:"minutes" validate:"required"`
+	CreatedBy    int		`json:"created_by,omitempty" form:"created_by" validate:"omitempty,numeric"`
+	CreatedAt    time.Time		`json:"created_at" form:"created_at" validate:"required"`
+}
--- aim/models/payroll.go
+++ aim/models/payroll.go
@@ -17,3 +17,20 @@ type Payroll struct{
 	CreatedAt    time.Time		`json:"created_at" validate:"required"`
 
 }
+
+
+type PayrollFull struct{
+	Id	int	`json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
+	CheckNo    int		`json:"check_no,omitempty" form:"check_no" validate:"omitempty,numeric"`
+	EmployeeRecordTitle    string		`json:"employee_record_id,omitempty" form:"employee_record_id" validate:"omitempty,numeric"`
+	PaymentCategoryTitle   string		`json:"payment_category_id,omitempty" form:"payment_category_id" validate:"omitempty,numeric"`
+	Amount    float64		`json:"amount,omitempty" form:"amount" validate:"omitempty,numeric"`
+	PreparedBy    int		`json:"prepared_by,omitempty" form:"prepared_by" validate:"omitempty,numeric"`
+	Descriptions    string		`json:"descriptions" form:"descriptions" validate:"required"`
+	PayMonth    string		`json:"pay_month" form:"pay_month" validate:"required"`
+	PayrollStatusTitle    string		`json:"payroll_status_id,omitempty" form:"payroll_status_id" validate:"omitempty,numeric"`
+	AttachmentTitle    string		`json:"attachment_id,omitempty" form:"attachment_id" validate:"omitempty,numeric"`
+	CreatedBy    int		`json:"created_by,omitempty"  validate:"omitempty,numeric"`
+	CreatedAt    time.Time		`json:"created_at" validate:"required"`
+
+}
