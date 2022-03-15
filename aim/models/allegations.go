6d4d8a5694d4c9609b59bf58c421c6809a23a4cf
cngirwa <cngirwa2007@gmail.com>
2022-03-15 14:04:20 +0300

    Update allegations.go

--- aim/models/allegations.go
+++ aim/models/allegations.go
@@ -6,17 +6,17 @@ import (
 
 //Allegation Structure
 type Allegation struct {
-	Id           int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
-	ActionTypeId int       `json:"action_type_id,omitempty" form:"action_type_id" validate:"omitempty,numeric"`
-	DateAccused  time.Time `json:"date_accused,omitempty" form:"date_accused" validate:"required"`
+	Id           int       `json:"id" form:"id" validate:"omitempty,numeric"`
+	ActionTypeId int       `json:"action_type_id" form:"action_type_id" validate:"omitempty,numeric"`
+	DateAccused  time.Time `json:"date_accused" form:"date_accused"`
 	CreatedBy    int       `json:"created_by,omitempty"`
 	CreatedAt    time.Time `json:"created_at,omitempty"`
 }
 
 type AllegationFull struct {
-	Id           int       `json:"id,omitempty" form:"id" validate:"omitempty,numeric"`
-	ActionTypeId string       `json:"action_type_id,omitempty" form:"action_type_id" validate:"omitempty,numeric"`
-	DateAccused  time.Time `json:"date_accused,omitempty" form:"date_accused" validate:"required"`
+	Id           int       `json:"id" form:"id" validate:"omitempty,numeric"`
+	ActionTypeId string       `json:"action_type_id" form:"action_type_id" validate:"omitempty,numeric"`
+	DateAccused  time.Time `json:"date_accused" form:"date_accused"`
 	CreatedBy    int       `json:"created_by,omitempty"`
 	CreatedAt    time.Time `json:"created_at,omitempty"`
 }
